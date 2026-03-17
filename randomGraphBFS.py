f1691c91baf6934222e52a3400f1482c31621c24
Jack Brunswik <jbrunswik5@gmail.com>
2026-03-17 17:25:14 -0400

    Now tracks max depth instead of edges examined

--- randomGraphBFS.py
+++ randomGraphBFS.py
@@ -1,11 +1,9 @@
-import random
 import networkx as nx
 from collections import deque
 
 class RandomGraphBFS:
     """ Random Graph BFS Simulation:
-    Generates a connected random graph and performs BFS traversal.
-    Designed for both Monte Carlo statistical experiments and real-time visualization. """
+    Generates a connected random graph and performs BFS traversal. Designed for both Monte Carlo statistical experiments and real-time visualization. """
 
     def __init__(self, p=0.1):
         self.p = p
@@ -25,29 +23,21 @@ class RandomGraphBFS:
         queue = deque()
         levels = {}
 
-        edges_examined = 0
-
         visited.add(start_node)
         queue.append(start_node)
         levels[start_node] = 0
 
         while queue:
-
             current = queue.popleft()
-
             for neighbor in G.neighbors(current):
-
-                edges_examined += 1  # Count every edge check
-
                 if neighbor not in visited:
                     visited.add(neighbor)
                     queue.append(neighbor)
                     levels[neighbor] = levels[current] + 1
 
-        tree_height = max(levels.values())
+        max_depth = max(levels.values())
 
         return {
-            "edges_examined": edges_examined,
             "nodes_visited": len(visited),
-            "tree_height": tree_height
+            "max_depth": max_depth
         }
